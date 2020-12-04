4ba61fe8046dfd3b152275577598c3b27094d685
Keenan Gray <keenangray@Keenans-MacBook-Pro-2.local>
2020-12-03 21:50:01 -0500

    half an update towards command-line execution from the server

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from subprocess import run
 from werkzeug.utils import secure_filename
+import random
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './uploads'
@@ -16,8 +17,13 @@ def index():
     form = UploadForm()
 
     if form.validate_on_submit():
-        filename = secure_filename(form.file.data.filename)
+        filename = "download" + random.randint(0,10000)  # secure_filename(form.file.data.filename)
         form.file.data.save('uploads/' + filename)
+
+#command to run is here
+#./FBX2gltf -b -i uploads/cube.fbx -o output/cube
+        run(["./FBX2gltf","-b","uploads/", filename, "uploads/")    
+
         return redirect(url_for('index'))
 
     return render_template('template.html', form=form)
