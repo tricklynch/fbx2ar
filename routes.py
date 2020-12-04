ed4fa56caed9c5f8f7fc8676ffe7979ae2714c9a
Patrick Lynch <p@tricklynch.org>
2020-12-03 20:08:32 -0500

    File upload works

--- routes.py
+++ routes.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, url_for
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
 from subprocess import run
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './uploads'
+app.config['SECRET_KEY'] = '\x00'
 
-@app.route('/', methods=['GET'])
+class UploadForm(FlaskForm):
+    file = FileField()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('template.html')
+    form = UploadForm()
 
-@app.route('/convert', methods=['POST'])
-def convert():
-    print(request.form[None])
-    return 'success'
+    if form.validate_on_submit():
+        filename = secure_filename(form.file.data.filename)
+        form.file.data.save('uploads/' + filename)
+        return redirect(url_for('index'))
+
+    return render_template('template.html', form=form)
 
 app.run('0.0.0.0', 8080, debug=True)
