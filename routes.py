e1ce337f7effc1870b3b82db5b5788be8b8c0f9c
EC2 Default User <ec2-user@ip-172-31-9-248.ec2.internal>
2020-12-07 23:57:14 +0000

    fbx 2 gltf working

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, send_from_directory
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from subprocess import run
 from werkzeug.utils import secure_filename
+from os import path
 import random
 
 app = Flask(__name__)
@@ -17,15 +18,21 @@ def index():
     form = UploadForm()
 
     if form.validate_on_submit():
-        filename = "download" + random.randint(0,10000)  # secure_filename(form.file.data.filename)
-        form.file.data.save('uploads/' + filename)
+        baseName = "download" + str( random.randint(0,10000))  # secure_filename(form.file.data.filename)
+        form.file.data.save('uploads/' + baseName+".fbx")
 
 #command to run is here
 #./FBX2gltf -b -i uploads/cube.fbx -o output/cube
-        run(["./FBX2gltf","-b","uploads/", filename, "uploads/")    
+        run(["./FBX2gltf","-b","-i",path.join("uploads",baseName + ".fbx"),"-o",path.join("output", baseName)])
 
-        return redirect(url_for('index'))
+        return redirect(url_for('sendFile',filename=baseName+".glb"))
 
     return render_template('template.html', form=form)
+
+
+@app.route('/output/<filename>')
+def sendFile(filename):
+    print(filename)
+    return send_from_directory("output",filename)
 
 app.run('0.0.0.0', 8080, debug=True)
